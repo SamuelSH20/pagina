857847b23c3f1e4aee6c361b9026de6715aab478
SamuelSH20 <157252437+SamuelSH20@users.noreply.github.com>
2024-01-20 17:51:15 -0500

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,8 @@
+alert("BIENVENIDO");
+var edad = 27;
+console.log(edad);
+
+document.getElementById('myButton').addEventListener('click', function() {
+    // Redirigir a otro archivo HTML (reemplaza 'otrapagina.html' con tu archivo deseado)
+    window.location.href = 'principal.html';
+});
